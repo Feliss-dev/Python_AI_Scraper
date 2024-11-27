86f1d66f21a8e372c49cf23f5679984d89fcaee6
Feliss-dev <thanhlta1234@gmail.com>
2024-11-27 09:11:13 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from scrape import (scrape_website, 
+                    split_dom_content,
+                    clean_body_content,
+                    extract_body_content,)
+
+from parse import parse_with_ollama
+
+st.title("AI Web Scraper")
+url = st.text_input("Enter URL: ")
+
+if st.button("Scrape Site"):
+    st.write("Scraping site...")
+    result = scrape_website(url)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("View DOM Element"):
+        st.text_area("DOM Content", cleaned_content, height=300)
+
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you to parse")
+
+    if st.button("Parse DOM"):
+       if parse_description:
+        st.write("Parsing DOM...")
+
+        dom_chunks = split_dom_content(st.session_state.dom_content)
+        result = parse_with_ollama(dom_chunks, parse_description)
+
+        st.write(result)
